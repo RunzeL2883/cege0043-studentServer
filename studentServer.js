a72ec189034e606181d8d489988c1ba526e906b7
RunzeL2883 <runze.liu.18@ucl.ac.uk>
2019-03-22 14:24:58 -0400

    upload practical 6 part 2 step 1

--- studentServer.js
+++ studentServer.js
@@ -1,21 +1,7 @@
-//process the uploaded data
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	next();
-});
-
 // express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
 var app = express();
-
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
@@ -23,17 +9,18 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
 
+
+//You should see ‘hello world from the HTTP server’
 app.get('/',function (req,res) {
 	res.send("hello world from the HTTP server");
 });
 
-// adding functionality to log the requests
-app.use(function (req, res, next) {
-	var filename = path.basename(req.url);
-	var extension = path.extname(filename);
-	console.log("The file " + filename + " was requested.");
-	next();
-});
+//process the uploaded data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 
 var fs = require('fs');
 var pg = require('pg');
@@ -63,6 +50,55 @@ app.get('/postgistest', function (req,res) {
 			res.status(200).send(result.rows);
 		});
 	});
+});
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+	var filename = path.basename(req.url);
+	var extension = path.extname(filename);
+	console.log("The file " + filename + " was requested.");
+	next();
+});
+
+app.post('/reflectData',function(req,res){
+    // note that we are using POST here as we are uploading data
+    // so the parameters form part of the BODY of the request rather
+    //than the RESTful API
+    console.dir(req.body);
+    // for now, just echo the request back to the client
+    res.send(req.body);
+});
+
+post('/uploadData',function(req,res){
+    // note that we are using POST here as we are uploading data
+    // so the parameters form part of the BODY of the request rather than the RESTful API
+    console.dir(req.body);
+    pool.connect(function(err,client,done) {
+    	if(err){
+    		console.log("not able to get connection "+ err);
+    		res.status(400).send(err);
+    	}
+    	var name = req.body.name;
+    	var surname = req.body.surname;
+    	var module = req.body.module;
+    	var portnum = req.body.port_id;
+    	var querystring = "INSERT into formdata (name,surname,module, port_id) values ($1,$2,$3,$4) ";
+    	console.log(querystring);
+    	client.query( querystring,[name,surname,module,portnum],function(err,result) {
+    		done();
+    		if(err){
+    			console.log(err);
+    			res.status(400).send(err);
+    		}
+    		res.status(200).send("row inserted");
+    	});
+    });
 });
 
 // serve static files - e.g. html, css
